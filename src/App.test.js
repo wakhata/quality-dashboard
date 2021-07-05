7398e4f759cf3c101425e63c3bc5d66e0a336bf0
Wakhata <wakhata@Wakhatas-Air.lan>
2021-07-05 20:50:20 +0300

    End of Welcome  Page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders coming soon", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h2Element = screen.getByText(/Hello world!!!/i);
+  expect(h2Element).toBeInTheDocument();
 });
